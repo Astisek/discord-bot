d22f1dfbb7440ba6c4039b3837b1945c48e8d4ce
Astisek <Astisek@hotmail.com>
2025-03-04 13:55:29 +0500

    fixed chunk size

--- src/utils/youtube.ts
+++ src/utils/youtube.ts
@@ -14,7 +14,7 @@ class Youtube {
   }
 
   getStream = (url: string) =>
-    this.innertube.download(url, { type: 'audio', quality: '360p', range: { start: 0, end: 8 * 1024 * 1024 * 3 } });
+    this.innertube.download(url, { type: 'audio', quality: '360p', range: { start: 0, end: 1024 * 1024 * 3 } }); // 3 MB
 
   // TODO: Форматьтировать с шортсов
   getFullUrl = (url: string) => {
