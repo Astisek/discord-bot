ab05bc2ffa56438c7b9cf3a2ad1bce91727f029a
Astisek <Astisek@hotmail.com>
2025-03-06 18:55:39 +0500

    fix

--- src/utils/songResourceFinder.ts
+++ src/utils/songResourceFinder.ts
@@ -3,8 +3,9 @@ import { Logger } from '@utils/logger';
 import { SGError } from '@utils/SGError';
 import { youtube } from '@utils/youtube';
 import internal, { PassThrough } from 'stream';
-import prism from 'prism-media';
 import { config } from '@utils/config';
+import { spawn } from 'child_process';
+import ffmpegPath from 'ffmpeg-static';
 
 class SongResourceFinder {
   private logger = new Logger('SongResourceFinder').childLogger;
@@ -25,38 +26,40 @@ class SongResourceFinder {
   };
 
   private createResourceFromReadableStream = async (stream: ReadableStream) => {
-    const transcoder = new prism.FFmpeg({
-      args: [
-        '-reconnect',
-        '1',
-        '-reconnect_streamed',
-        '1',
-        '-reconnect_on_network_error',
-        '1',
-        '-reconnect_delay_max',
-        '30',
-        '-analyzeduration',
-        '0',
-        '-loglevel',
-        '0',
-        '-f',
-        's16le',
-        '-ar',
-        '48000',
-        '-ac',
-        '2',
-      ],
-    });
-    const readableStream = internal.Readable.fromWeb(stream, { objectMode: false });
-    readableStream.pipe(transcoder).pipe(
-      new PassThrough({
-        highWaterMark: config.chunkSize * 1.5,
-      }),
-    );
+    const ffmpegProcess = spawn(ffmpegPath || '', [
+      '-reconnect',
+      '1',
+      '-reconnect_streamed',
+      '1',
+      '-reconnect_on_network_error',
+      '1',
+      '-reconnect_on_http_error',
+      '4xx,5xx',
+      '-reconnect_delay_max',
+      '30',
+      '-analyzeduration',
+      '0',
+      '-loglevel',
+      'error',
+      '-i',
+      'pipe:0',
+      '-f',
+      's16le',
+      '-ar',
+      '48000',
+      '-ac',
+      '2',
+      'pipe:1',
+    ]);
+    const inputStream = internal.Readable.fromWeb(stream, { objectMode: false });
+    const outputStream = new PassThrough({ highWaterMark: config.chunkSize * 1.5 });
 
-    readableStream.on('error', (e) => this.logger.debug(`Resource error ${e.message} ${e.stack}`));
+    inputStream.pipe(ffmpegProcess.stdin);
+    ffmpegProcess.stdout.pipe(outputStream);
 
-    return createAudioResource(readableStream);
+    outputStream.on('error', (e) => this.logger.debug(`Resource error ${e.message} ${e.stack}`));
+
+    return createAudioResource(outputStream);
   };
 }
 
