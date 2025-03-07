48df97aefe34d7972e0ea5d116cf19a7d7b29896
Astisek <Astisek@hotmail.com>
2025-03-08 01:27:24 +0500

    fix

--- src/utils/songResourceFinder.ts
+++ src/utils/songResourceFinder.ts
@@ -1,4 +1,5 @@
 import { createAudioResource, StreamType } from '@discordjs/voice';
+import { config } from '@utils/config';
 import { Logger } from '@utils/logger';
 import { SGError } from '@utils/SGError';
 import { youtube } from '@utils/youtube';
@@ -23,24 +24,9 @@ class SongResourceFinder {
   };
 
   private createResourceFromReadableStream = async (stream: ReadableStream<Uint8Array<ArrayBufferLike>>) => {
-    const readableStream = Readable.fromWeb(stream);
-    // const passThrough = new PassThrough({
-    //   highWaterMark: config.chunkSize,
-    //   writableHighWaterMark: config.chunkSize,
-    //   readableHighWaterMark: config.chunkSize,
-    //   allowHalfOpen: true,
-    //   autoDestroy: false,
-    //   decodeStrings: false,
-    // });
+    const readableStream = Readable.fromWeb(stream, { highWaterMark: config.chunkSize });
 
-    // const passThroughStream = readableStream.pipe(passThrough);
-
-    // passThrough.on('end', () => this.logger.error(`passThrough end`));
-    // passThrough.on('close', () => this.logger.error(`passThrough close`));
-    // passThrough.on('error', (e) => this.logger.error(`passThrough ${e.message} ${e.stack}`));
-    // readableStream.on('error', (e) => this.logger.error(`readableStream ${e.message} ${e.stack}`));
-    // readableStream.on('close', () => this.logger.error(`readableStream close`));
-    // readableStream.on('end', () => this.logger.error(`readableStream end`));
+    readableStream.on('error', (e) => this.logger.error(`readableStream ${e.message} ${e.stack}`));
 
     return createAudioResource(readableStream, { inputType: StreamType.Arbitrary });
   };
