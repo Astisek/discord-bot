9a3375a887cbcd5b2fbe06c6c10fb5364d3a3c5a
Astisek <Astisek@hotmail.com>
2025-03-07 12:25:58 +0500

    fix

--- src/utils/songResourceFinder.ts
+++ src/utils/songResourceFinder.ts
@@ -1,5 +1,4 @@
 import { createAudioResource, demuxProbe } from '@discordjs/voice';
-import { config } from '@utils/config';
 import { Logger } from '@utils/logger';
 import { SGError } from '@utils/SGError';
 import { youtube } from '@utils/youtube';
@@ -24,9 +23,9 @@ class SongResourceFinder {
   };
 
   private createResourceFromReadableStream = async (stream: ReadableStream) => {
-    const readableStream = Readable.fromWeb(stream, { highWaterMark: config.chunkSize * 0.5 });
+    const readableStream = Readable.fromWeb(stream, { highWaterMark: 1 });
     const passThrough = new PassThrough({
-      highWaterMark: config.chunkSize * 0.5,
+      highWaterMark: 1,
       allowHalfOpen: true,
       autoDestroy: false,
       decodeStrings: false,
@@ -34,8 +33,12 @@ class SongResourceFinder {
 
     const passThroughStream = readableStream.pipe(passThrough);
 
+    passThrough.on('end', () => this.logger.error(`passThrough end`));
+    passThrough.on('close', () => this.logger.error(`passThrough close`));
     passThrough.on('error', (e) => this.logger.error(`passThrough ${e.message} ${e.stack}`));
     readableStream.on('error', (e) => this.logger.error(`readableStream ${e.message} ${e.stack}`));
+    readableStream.on('close', () => this.logger.error(`readableStream close`));
+    readableStream.on('end', () => this.logger.error(`readableStream end`));
 
     const { stream: discordStream, type } = await demuxProbe(passThroughStream);
 
